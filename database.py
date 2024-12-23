f1b3817a01163540dbbb904b61dbc9111c07bfdc
Chappier637 <polegleb2001@gmail.com>
2024-12-23 05:06:33 +0300

    todo_app

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ SQLITE_DATABASE_URL = 'sqlite:///./data/todo.db'
 engine = create_engine(
     url = SQLITE_DATABASE_URL,
     connect_args={'check_same_thread' : False}
-)
+) 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, Boolean
 from database import Base
 
 class ToDoItem(Base):
-    __tablename__ = 'todo_items'
+    __tablename__ = 'todo_items' 
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
